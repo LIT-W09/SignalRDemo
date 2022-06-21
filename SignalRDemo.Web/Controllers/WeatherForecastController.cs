f1c7c37e6287bd024c2166e6711a839df1c58ac8
LIT-W09 <99372455+LIT-W09@users.noreply.github.com>
2022-06-20 20:12:43 -0400

    Update WeatherForecastController.cs

--- SignalRDemo.Web/Controllers/WeatherForecastController.cs
+++ SignalRDemo.Web/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace SignalRDemo.Web.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            Console.WriteLine("Hello from git!");
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
